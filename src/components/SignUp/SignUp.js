e9aa08561e53dd312c047900f25a891b05d5c0e4
galina <g.matyasheva@mail.ru>
2020-10-21 13:07:51 +0300

    SignUp validation, error messages and highlighting

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -5,6 +5,11 @@ import api from "../../api";
 
 
 class SignUp extends Component {
+    state = {
+        errorMessageEmail: '',
+        errorMessagePassword: ''
+    };
+
     onClickRegister = async ()=> {
         //'------------onClickLogin'
         const payload = { name: this.state.name, password: this.state.password, email: this.state.email};//передаем на сервер
@@ -27,15 +32,45 @@ class SignUp extends Component {
 
     onChangeEmail= e => {
        //'----------onChangeEmail'
+
+        const validateResalt = this.validateEmail(e.target.value);
+        console.log(validateResalt, e.target.value);
+        if (!this.validateEmail(e.target.value)) {
+            this.state.errorMessageEmail = 'invalid email';
+        } else {
+            this.state.errorMessageEmail = '';
+        }
         this.setState({
             email: e.target.value
         })
+
+
+    };
+
+    validateEmail = (email) => {
+        if (/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i.test(email)){
+            return true;
+        } else {
+            return false;
+        }
     };
 
     onChangePassword = e => {
         //'----------onChangePassword'
         this.setState({
             password: e.target.value
+        })
+    };
+
+    onChangeRepeatPassword = e => {
+        //'----------onChangePassword'
+       if (this.state.password === e.target.value){
+       this.state.errorMessagePassword = '';
+    } else {
+           this.state.errorMessagePassword = 'passwords are not the same';
+       }
+        this.setState({
+            repeatPassword: e.target.value
         })
     };
 
@@ -59,17 +94,15 @@ class SignUp extends Component {
                         <input className='name' type="text" placeholder="Enter Name" name="name" required onChange={(e)=>this.onChangeName(e)}/>
 
                             <label htmlFor="email"><b>Email</b></label>
-                            <input className='email' type="text" placeholder="Enter Email" name="email" required onChange={(e)=>this.onChangeEmail(e)}/>
+                            <input className={this.state.errorMessageEmail === ''? 'email' : 'mistake-email'} type="text" placeholder="Enter Email" name="email" required onChange={(e)=>this.onChangeEmail(e)}/>
+                            <p className={this.state.errorMessageEmail === ''? 'error-message' : 'message'}>{this.state.errorMessageEmail}</p>
 
                             <label htmlFor="psw"><b>Password</b></label>
                             <input className='register-password' type="password" placeholder="Enter Password" name="psw" required onChange={(e)=>this.onChangePassword(e)}/>
 
                             <label htmlFor="psw-repeat"><b>Repeat Password</b></label>
-                            <input className='register-password' type="password" placeholder="Repeat Password" name="psw-repeat" required/>
-                             <p></p>
-                            {/*<label htmlFor='remember'>*/}
-                        {/*<input className='register-remember' type="checkbox"  name="remember"/> Remember me*/}
-                            {/*</label>*/}
+                            <input className= {this.state.errorMessagePassword === ''? 'register-password' : 'mistake-repeat-password'} type="password" placeholder="Repeat Password" name="psw-repeat" required  onChange={(e)=>this.onChangeRepeatPassword(e)}/>
+                            <p className={this.state.errorMessagePassword === ''? 'error-message' : 'message'}>{this.state.errorMessagePassword}</p>
 
                             <p>By creating an account you agree to our <a href="#">Terms
                                 & Privacy</a>.</p>
